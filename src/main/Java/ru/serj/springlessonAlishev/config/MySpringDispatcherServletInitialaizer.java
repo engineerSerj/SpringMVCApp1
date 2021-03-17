cf19f8874140aadda838fb630ccb4c45bb34e1d8
engineerSerj <serg-smile@yandex.ru>
2021-03-17 21:43:12 +0300

    Spring WEB App test with java config

--- src/main/Java/ru/serj/springlessonAlishev/config/MySpringDispatcherServletInitialaizer.java
+++ src/main/Java/ru/serj/springlessonAlishev/config/MySpringDispatcherServletInitialaizer.java
@@ -2,8 +2,10 @@ package ru.serj.springlessonAlishev.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 //AbstractAnnotationConfigDispatcherServletInitializer реализует интерфейс WebApplicationInitializer поэтому можем им заменить web.xml
+// это класс который исполняет роль Spring  web.xml
 public class MySpringDispatcherServletInitialaizer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
+    //использовать небудем, возвращаем null
     protected Class<?>[] getRootConfigClasses() {
         return new Class[0];
     }
@@ -11,11 +13,14 @@ public class MySpringDispatcherServletInitialaizer extends AbstractAnnotationCon
     @Override
     //в этих классах должны передать какую то конфигурацию, конкретно в этот класс должны передать класс java замену applicationContextMVC.xml(spring config)
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        //подставляем наш Spring config класс
+        return new Class[] {SpringConfig.class}; // создаем массив типа Class и инициализируем его одни нашим Spring config классом
     }
 
     @Override
+    //эквивалентно что мы в файле web.xml все http запросы пользователя посылаем на диспетчер сервлет
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
+//и добавим зависимость javax Servlet API в pom эта зависимость используется AbstractAnnotationConfigDispatcherServletInitializer
--- src/main/Java/ru/serj/springlessonAlishev/config/SpringConfig.java
+++ src/main/Java/ru/serj/springlessonAlishev/config/SpringConfig.java
@@ -11,14 +11,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-
+// SpringConfig заменяет нам applicationContextMVC.xml
 @Configuration
 @ComponentScan("ru.serj.springlessonAlishev")
-@EnableWebMvc // т.к. у нас Spring MVC приложение, которое поддерживает веб функции, эта аннотация равноценна тэгу <mvc:annotation-driven/> <!--включает необходимые аннотации для Spring MVC приложения-->
+@EnableWebMvc // т.к. у нас Spring MVC приложение, которое поддерживает веб функции, эта аннотация равноценна
+// тэгу <mvc:annotation-driven/> <!--включает необходимые аннотации для Spring MVC приложения-->
 public class SpringConfig implements WebMvcConfigurer {
-// implements WebMvcConfigurer
+// implements WebMvcConfigurer с этим интерфейсом мы реализуем метод configureViewResolvers этот интерфейс реализуется
+// в том случаем когда мы хотим под себя настроить Spring VMC,
+// в данном случае мы хотим вместо стандартного шаблонизатора использовать шаблонизатор Thymeleaf
     private final ApplicationContext applicationContext;
-
+// внедряем applicationContext с помощью @Autowired
     @Autowired
     public SpringConfig(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
@@ -27,20 +30,21 @@ public class SpringConfig implements WebMvcConfigurer {
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        //используем тут бин контекста чтобы настроить Thymeleaf
         templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("/WEB-INF/views/");
-        templateResolver.setSuffix(".html");
+        templateResolver.setPrefix("/WEB-INF/views/");// папка с нашими представлениями
+        templateResolver.setSuffix(".html");//их расширения
         return templateResolver;
     }
 
     @Bean
-    public SpringTemplateEngine templateEngine() {
+    public SpringTemplateEngine templateEngine() {//бин где производим конфигурацию наших представлений
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
     }
-
+//передаем инфу спринг мвс что хотим использовать шаблонизатор Thymeleaf
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
