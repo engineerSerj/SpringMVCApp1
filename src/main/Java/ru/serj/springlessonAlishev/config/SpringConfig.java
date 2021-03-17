d965aa9bebe536fca8fd333c8263d496e2ad067c
engineerSerj <serg-smile@yandex.ru>
2021-03-17 18:24:31 +0300

    Spring WEB App test with java config

--- src/main/Java/ru/serj/springlessonAlishev/config/SpringConfig.java
+++ src/main/Java/ru/serj/springlessonAlishev/config/SpringConfig.java
@@ -1,14 +1,22 @@
 package ru.serj.springlessonAlishev.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 @ComponentScan("ru.serj.springlessonAlishev")
 @EnableWebMvc // т.к. у нас Spring MVC приложение, которое поддерживает веб функции, эта аннотация равноценна тэгу <mvc:annotation-driven/> <!--включает необходимые аннотации для Spring MVC приложения-->
-public class SpringConfig {
-
+public class SpringConfig implements WebMvcConfigurer {
+// implements WebMvcConfigurer
     private final ApplicationContext applicationContext;
 
     @Autowired
