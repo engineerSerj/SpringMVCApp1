c6a82a2644030bd9f28db900cd0bafe0a5792032
engineerSerj <serg-smile@yandex.ru>
2021-03-18 04:35:45 +0300

    fixup! Spring WEB App test with java config

--- src/main/Java/ru/serj/springlessonAlishev/controllers/FirstController.java
+++ src/main/Java/ru/serj/springlessonAlishev/controllers/FirstController.java
@@ -3,15 +3,34 @@ package ru.serj.springlessonAlishev.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){ //String значит возвращаем text/html
+    public String helloPage(@RequestParam(value="name", required = false) String name, //если не передадим параметры в URL получим ошибку 400 (неверный запрос) в отличие от второго способа где вернулся бы null
+                            @RequestParam(value="surname", required = false) String surname){// или можем в этой аннотации использовать конструкцию @RequestParam(value="surname", required = false), это означает что если мы в гет запросе передаем параметры то эти параметры внедряються в наши переменные, если не предаем то просто передается null
+        // значение которое лежит по ключу name будет положено в параметр name
+        //Spring за нас возьмет значения из URL с этими ключами, положит их в эти параметры метода и эти параметры внутри метода уже будут содержать в себе значения
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
+
+    /*@GetMapping("/hello")
+    public String helloPage(HttpServletRequest request){// если не передадим параметры в URL, то String name и surname будут равны null
+        //request этот объект будет внедрен Spring и в этом объекте содержаться все
+        // сведения о поступающем HTTP запросе от пользователя
+        //давайте скажем что в url пользователь должен будет предавать два параметра имя и фамилия
+        String name = request.getParameter("name"); //по ключ name получаем значение и присваиваем его String name
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
+        return "first/hello";
+    }*/
 
     @GetMapping("/goodbye")
     public String goodByePage(){
