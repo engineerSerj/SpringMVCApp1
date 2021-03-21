bfbeb0ac21b5bf62e9f4c7ba62877d83b1cc0d87
engineerSerj <serg-smile@yandex.ru>
2021-03-21 23:47:41 +0300

    add Validate (Hibernate-validator)

--- src/main/Java/ru/serj/springlessonAlishev/controllers/PeopleController.java
+++ src/main/Java/ru/serj/springlessonAlishev/controllers/PeopleController.java
@@ -1,5 +1,6 @@
 package ru.serj.springlessonAlishev.controllers;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
