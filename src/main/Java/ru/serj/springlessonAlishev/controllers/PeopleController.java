5144c13b1440ecce3e27cab9f739b348373fc728
engineerSerj <serg-smile@yandex.ru>
2021-03-21 13:50:37 +0300

    add patch method

--- src/main/Java/ru/serj/springlessonAlishev/controllers/PeopleController.java
+++ src/main/Java/ru/serj/springlessonAlishev/controllers/PeopleController.java
@@ -52,5 +52,19 @@ public class PeopleController {
         // используем редирект - механизм браузера который говорит перейти на какую то другую страницу
         return "redirect:/people";
     }
+    //Создадим метод возвращающий страницу для редактирования человека
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id){
+            model.addAttribute("person", personDAO.show(id));//внедряем в модель человека с этим id , его будем отображать на форме редактирования и к нему будет доступ в нашем представлении, чтобы в самой форме поля уже небыли пустыми
+            return "people/edit";
+    }
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person")Person person, @PathVariable("id") int id){
+     //принимаем с помощью @ModelAttribute("person")объект person из формы, @PathVariable("id") принимает id из адреса
+     //теперь мы должны найти человека с таким id в бд и заменить его значения на те что пришли из формы, на те что лежат в person полученном с помощью @ModelAttribute
+      // это будем делать внутри DAO
+        personDAO.update(id, person);
+        return  "redirect:/people";
+    }
 
 }
